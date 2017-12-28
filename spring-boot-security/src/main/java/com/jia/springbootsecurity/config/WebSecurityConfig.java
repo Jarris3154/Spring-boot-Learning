195a8a6957e9628a12685d6580b46a19da012890
Hu, Jia <Hu.Jia.Jarris@Vertivco.com>
2017-12-28 17:41:56 +0800

    refactor: implement security
    Notes:
    1. 建议在WebSecurity中先disable掉CSRF，不然会影响到正常运行；
    2. 注意： freemarker中判断url中的参数用
    <#if RequestParameters["error"]?? ></#if>
    3. 注意： freemarker中获取当前session中SpringSecurity配置的的登陆用户用
    
    ${Session.SPRING_SECURITY_CONTEXT.authentication.principal.username!'xxx'}来获取

--- spring-boot-security/src/main/java/com/jia/springbootsecurity/config/WebSecurityConfig.java
+++ spring-boot-security/src/main/java/com/jia/springbootsecurity/config/WebSecurityConfig.java
@@ -23,6 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .logout()
         .permitAll();
+
+        http.csrf().disable();
     }
 
     @Autowired
